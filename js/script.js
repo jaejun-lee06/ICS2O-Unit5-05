a80687cb25acb0a0fabd2bca215cdf81d38800d6
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-05-13 16:47:43 +0000

    negative

--- js/script.js
+++ js/script.js
@@ -9,17 +9,20 @@ function myButtonClicked() {
   const side2 = document.getElementById("side2").value 
   const side3 = document.getElementById("side3").value
 
-  
-  if ((side1 == side2 && side2 == side3)) {
+  if ((side1 < 0 || side2 < 0 || side1 < 0)) {
+    document.getElementById("answers").innerHTML = "Please put a postive number. "
+  }
+  else if ((side1 == side2 && side2 == side3)) {
     document.getElementById("answers").innerHTML = "The triangle is an equilateral triangle. "
   }
   else if ((side1 == side2 || side2 == side3 || side1 == side3)) {
     document.getElementById("answers").innerHTML = "The triangle is an isosceles triangle. "
   }
-  else if ((side1 <= 0 || side2 <= 0 || side1 <= 0)) {
-    document.getElementById("answers").innerHTML = "This is not a triangle. Please put a positive number. "
+  else if ((side1 != side2 && side2 != side3 && side3 != side1)) {
+    document.getElementById("answers").innerHTML = "The triangle is a scalene triangle. "
   }
   else {
-    document.getElementById("answers").innerHTML = "The triangle is a scalene triangle."
+    document.getElementById("answers").innerHTML = "This is not a triangle. "
   }
 }
+
